eb1a8351575684b85112cf6e01a9da640b9fa1a7
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-29 20:18:52 +0530

    [vidyadhar hudge]Add:Welcome message

---
+++ AddressBookUsingAdo.Net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookUsingAdo.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to address book using ado.net");
+        }
+    }
+}
